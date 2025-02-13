8101f273787b8b59138ee65e45699079644e6fa7
RafaBarezi <rafaela.vieira.barezi@gmail.com>
2025-02-13 14:25:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value;
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+
+    const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+    if (!regex.test(nome)) {
+        alert("O nome não deve conter números ou caracteres especiais.");
+        return;
+    }
+
+    // Impedindo duplicidade de registros:
+    if (listaAmigos.includes(nome)) {
+        alert("Este nome já foi incluído na sua lista.");
+        return;
+    }
+
+    listaAmigos.push(nome);
+
+    atualizarLista();
+
+    input.value = "";
+}
+
+function atualizarLista() {
+    const ul = document.getElementById("listaAmigos"); 
+    ul.innerHTML = ""; 
+
+    listaAmigos.forEach((nome) => {
+        const li = document.createElement("li"); 
+        li.textContent = nome;
+        ul.appendChild(li); 
+    });
+}
+
+function sortearAmigo() {
+
+    if (listaAmigos.length < 2) {
+        alert("Adicione pelo menos três nomes antes de sortear.");
+        return;
+    }
+
+    const sorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+
+    const resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = `<li> O amigo sorteado foi: <strong>${sorteado}</strong>!</li>`;
+}
