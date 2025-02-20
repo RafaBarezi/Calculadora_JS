4c71061ab72011e6d38ca8a1aa5fd33c3b6000d1
RafaBarezi <rafaela.vieira.barezi@gmail.com>
2025-02-19 22:26:42 -0300

    first commit

--- app.js
+++ app.js
@@ -1,63 +1,50 @@
-// Inicie declarando uma variável do tipo array, que armazenará os nomes dos amigos inseridos. 
-let amigos = [];
+// Confirmando se todos os campos estão preenchidos:
+function verificarCampos() {
+    const num1 = document.getElementById("numero1").value;
+    const num2 = document.getElementById("numero2").value;
+    const operador = document.getElementById("operador").value;
+    const botaoCalcular = document.getElementById("calcular");
 
-//Desenvolva uma função que permita ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos criada anteriormente:
-function adicionarAmigo() {
-    //Capturar o valor do campo de entrada: Utilize document.getElementById ou document.querySelector para obter o texto inserido pelo usuário:
-    let input = document.getElementById("amigo");
-    let nome = input.value;
-
-    //Validar a entrada: Implemente uma validação para garantir que o campo não esteja vazio:
-    if (nome === "") {
-        alert("Por favor, insira um nome.");
-        return;
-    }
-
-    const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
-    if (!regex.test(nome)) {
-        alert("O nome não deve conter números ou caracteres especiais.");
-        return;
-    }
-
-    if (amigos.includes(nome)) {
-        alert("Este nome já foi incluído na sua lista.");
-        return;
-    }
-
-    // Atualizar o array de amigos: Se o valor for válido, adicione-o ao array que armazena os nomes dos amigos usando o método .push():
-    amigos.push(nome);
-
-    atualizarLista();
-
-    //Limpar o campo de entrada: Após adicionar o nome, redefina o campo de texto para uma string vazia.
-    input.value = "";
+    // Habilitando o botão se os dois números e o operador forem informados
+    botaoCalcular.disabled = !(num1 !== "" && num2 !== "" && operador !== "");
 }
 
-function atualizarLista() {
-    const ul = document.getElementById("amigos");
-    ul.innerHTML = "";
+// função para fazer o calculo
+function calcular() {
+    const num1 = parseFloat(document.getElementById("numero1").value);
+    const num2 = parseFloat(document.getElementById("numero2").value);
+    const operador = document.getElementById("operador").value;
+    let resultado;
 
-    amigos.forEach((nome) => {
-        const li = document.createElement("li");
-        li.textContent = nome;
-        ul.appendChild(li);
-    });
-}
+    switch (operador) {
+        case "+":
+            resultado = num1 + num2;
+            break;
+        case "-":
+            resultado = num1 - num2;
+            break;
+        case "*":
+            resultado = num1 * num2;
+            break;
+        case "/":
+            if (num2 === 0) {
+                resultado = " um número não pode ser dividido por zero";
+            } else {
+                resultado = (num1 / num2).toFixed(2).replace(".", ","); // Exibindo apenas duas casas na divisão e ainda substituindo o ponto por vírgula
+            }
+            break;
+        default:
+            resultado = "Erro: operador inválido.";
 
-function sortearAmigo() {
-
-    //Validar que há amigos disponíveis: Antes de sortear, verificar se o array amigos não está vazio:
-    if (amigos.length <= 1) {
-        alert("Adicione pelo menos um nome antes de sortear.");
-        return;
     }
 
-    // Gerar um índice aleatório: Usar Math.random() e Math.floor() para selecionar um índice aleatório do array.
-    const indice = Math.floor(Math.random() * amigos.length);
-    console.log("Índice sorteado:", indice);
-    const sorteado = amigos[indice];
-
-    // Mostrar o resultado: Atualizar o conteúdo do elemento de resultado usando document.getElementById()
-    const resultado = document.getElementById("resultado");
-    resultado.innerHTML = `<li> O amigo sorteado foi: <strong>${sorteado}</strong>!</li>`;
+    // exibindo o resultado nos dois campos
+    document.getElementById("resultado").innerText = "O resultado é : " + resultado;
 }
+
+// registra os eventos somente após o carregamento do DOM. Ou seja, estas instruções vão me permitir alterar n°s apśo informados e fazer novas contas.  
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("numero1").addEventListener("input", verificarCampos);
+    document.getElementById("numero2").addEventListener("input", verificarCampos);
+    document.getElementById("operador").addEventListener("change", verificarCampos);
+});
